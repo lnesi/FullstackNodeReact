fb9b45d498c60d694ee144988031216732ff3cd1
Luis Nesi <lenesi@hotmail.com>
2017-10-31 14:42:15 +0000

    oauth google

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
 
-    res.send({
-        hi: "there"
-    });
-});
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
-console.log("SERVER STARTED at port "+PORT);
+console.log("SERVER STARTED at port " + PORT);
