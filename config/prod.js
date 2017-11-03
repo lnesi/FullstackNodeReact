069f506f356c0eb515e1ac80963320985653797b
Luis Nesi <luisenesi@gmail.com>
2017-11-03 15:57:36 +0000

    env

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+  googleClientID:process.env.googleClientID,
+  googleClientSecret:process.env.googleClientSecret,
+  mongoURI:process.env.mongoURI,
+  cookieKey:process.env.cookieKey
+}
