2296c83e7b57992868f3deea2d8d6b767656cf78
Luis Nesi <lenesi@hotmail.com>
2017-11-15 17:16:29 +0000

    Development

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 	renderContent(){
@@ -8,25 +9,22 @@ class Header extends Component {
 			case null:
 				return;
 			case false:
-				return(
-					<li>
-						<a href="/auth/google">Login With Google</a>
-					</li>
-				)
+				return <li><a href="/auth/google">Login With Google</a></li>;
 			default:
-				return(
-					<li><a href="/api/logout">Logout</a></li>
-				)
+				return [
+				<li key={1}><Payments/></li>,
+				<li key={2} style={{margin:'0 10px'}}>Credists: {this.props.auth.credits}</li>,
+				<li key={3}><a href="/api/logout">Logout</a></li>
+				];
 
 		}
 		
 	}
 	render() {
-
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<Link to="/" className="left brand-logo">Emaily</Link>
+					<Link to={this.props.auth?'/surveys':'/'} className="left brand-logo">Emaily</Link>
 					<ul className="right">
 						{this.renderContent()}
 					</ul>
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 
@@ -14,6 +15,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
 	cookieSession({
 		maxAge:30*24*60*60*1000, //30 days
@@ -24,11 +27,22 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 //Routes
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
-
-
+// Client Serve in production
+if(process.env.NODE_ENV==="production"){
+	//Express will serve production assets
+	app.use(express.static('client/build'));
+	
+	//Express will serve index.html of client if does not recognize the route
+	const path=require('path');
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+	});
+}
 
 // Server Start
 const PORT = process.env.PORT || 5000;
